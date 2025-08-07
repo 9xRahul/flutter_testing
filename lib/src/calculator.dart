e8a5b05b2f703a1c8dfa5df8ad29b648edfca11a
rahul <rahul.p@9xtechnology.com>
2025-08-07 11:31:28 +0530

    group,setup,async await

--- lib/src/calculator.dart
+++ lib/src/calculator.dart
@@ -1,6 +1,15 @@
 class Calculator {
   double add(double a, double b) => a + b;
   double subtract(double a, double b) => a - b;
-  double divide(double a, double b) => a / b;
+  double divide(double a, double b) {
+    if (b == 0) throw ArgumentError("canot divide by zero");
+
+    return a / b;
+  }
+
   double multiply(double a, double b) => a * b;
+
+  Future<double> powerOfTwo(double a) {
+    return Future.delayed(Duration(seconds: 1), () => a * a);
+  }
 }
--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -2,14 +2,56 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:testing_flutter_apps/src/calculator.dart';
 
 void main() {
-  test("the cakculator return 4 when adding 2 and 2", () {
-    //step 1 - setup phase =>initilize the object
-    final calculator = Calculator();
+  // test("the cakculator return 4 when adding 2 and 2", () {
+  //   //step 1 - setup phase =>initilize the object
 
-    //step2 - side effects => collect the result we want to test
-    final result = calculator.add(2, 3);
+  //   //step2 - side effects => collect the result we want to test
+  //   final result = calculator.add(2, 3);
 
-    //step 3 => expectations => compare the expected value with the result got
-    expect(result, 5);
+  //   //step 3 => expectations => compare the expected value with the result got
+  //   expect(result, 5, reason: "The test results must be exactly 5");
+  // });
+
+  late Calculator calculator;
+
+  setUp(() {
+    print("initilized the calculator object");
+    calculator = Calculator();
+  });
+
+  group('Addition', () {
+    test('2 + 3 = 5', () => expect(calculator.add(2, 3), 5));
+    test('0 + 0 = 0', () => expect(calculator.add(0, 0), 0));
+    test('-5 + 10 = 5', () => expect(calculator.add(-5, 10), 5));
+  });
+
+  group('Subtraction', () {
+    test('5 - 3 = 2', () => expect(calculator.subtract(5, 3), 2));
+    test('0 - 0 = 0', () => expect(calculator.subtract(0, 0), 0));
+    test('-5 - 5 = -10', () => expect(calculator.subtract(-5, 5), -10));
+  });
+
+  group('Multiplication', () {
+    test('2 * 3 = 6', () => expect(calculator.multiply(2, 3), 6));
+    test('0 * 1000 = 0', () => expect(calculator.multiply(0, 1000), 0));
+    test('-2 * -3 = 6', () => expect(calculator.multiply(-2, -3), 6));
+    test('-2 * 3 = -6', () => expect(calculator.multiply(-2, 3), -6));
+  });
+
+  group('Division', () {
+    test('6 / 2 = 3', () => expect(calculator.divide(6, 2), 3));
+    test('0 / 5 = 0', () => expect(calculator.divide(0, 5), 0));
+    test('5 / 2 = 2.5', () => expect(calculator.divide(5, 2), 2.5));
+
+    // OR if you want to throw exception on division by 0
+    test('throws exception on divide by 0', () {
+      expect(() => calculator.divide(5, 0), throwsArgumentError);
+    });
+  });
+
+  group("Finding the power of two", () {
+    test('return s 9 if value is 3', () async {
+      expect(await calculator.powerOfTwo(3), 9);
+    });
   });
 }
